26479f6d66d9b49b80a23cde175b06a1ac65324e
divinefavourak <divinefavourakanbi07@gmail.com>
2025-03-16 22:36:07 +0100

    update

--- sct_backend/sct_backend/views.py
+++ sct_backend/sct_backend/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def home(request):
+    return render(request, "index.html")
